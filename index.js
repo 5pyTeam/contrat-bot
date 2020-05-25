732d720a8aaa402fb9842ea08046274645bb601d
yoyozbi <yohan.zbinden@gmail.com>
2020-05-25 14:19:42 +0200

    added contrat.add and update commands

--- index.js
+++ index.js
@@ -6,45 +6,61 @@ const prefix = '!';
 const dataPath = 'data.json'
 let jsonData = JSON.parse(fs.readFileSync(dataPath));
 
+const defaultEmbed = new Discord.MessageEmbed()
+    .setTitle('commande non valide')
+    .setDescription('la commande spécifée n\'est pas valide, veuillez spécififer les paramètres ci-dessous');
+const commandsWithParams = {"contrat.add":
+                        [
+                            {"name":"mention de l'utilisateur","description": "mention de l'utilisateur qui vient de remplir son contrat"},
+                            {"name":"durée du contrat", "description":"durée du contrat du membre(**m** pour mois et **y** pour année)"}
+                        ]
+                    }
 client.once('ready', () => {
     console.log('Ready!');
 });
-
+const regexDate = /\d(y|m)/;
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const content = message.content;
     const member = message.guild.member(message.member);
     const guild = message.guild;
-    const memberMentions = message.mentions.members;
+    const memberMentions = message.mentions.users;
     const roleMentions = message.mentions.roles;
     const params = message.content.slice(prefix.length).split(' ');
     const command = params.shift().toLowerCase();
+    console.log(params);
     if(command == 'ping')
     {
         message.reply('pong');
-    }else if(command == 'contrat.add')
+    }else if(command == 'contrat.add' || command == 'contrat.update')
     {
-        if(memberMentions.first() && params[2])
+        if(params.length == 2 && params[1].match(regexDate) && memberMentions.size == 1)
         {
-
+            const name = memberMentions.first().username;
+            const data = JSON.parse(`{"date" : "${new Date().toString()}", "length": "${params[1]}"}`);
+            changeJson(name,data);
         }else
         {
-            message.channel.send({
-                embed: {
-                    title: "la commande n'est pas valide",
-                    description: "veuillez spécifier tous le paramètes listés ci-dessous",
-                    fields: [{
-                            description: "*utilisateur:* mention de l'utilisateur qui vient de remplir sont contrat",                            
-                        },
-                        {
-                            description: "*durée du contrat:* durée du contrat du joueur(m pour mois et y pour année)"
-                        },
-                    ],                    
-                }
-            });
+            showHelp(message.channel,'contrat.add');
         }
     }
 });
+function showHelp(channel, command)
+{
+    const newEmbed = new Discord.MessageEmbed(defaultEmbed);
+    for(var value in commandsWithParams)
+    {
+        var g = commandsWithParams[value];
+        if(command == value)
+        {
+            for(var i = 0; i <  g.length; i++)
+            {
+                newEmbed.addField(g[i].name,g[i].description, true);
+            }
+        }
+    }
+    channel.send(newEmbed);
+}
 function changeJson(rank, data)
 {
     jsonData[rank] = data;
